b9a105ff9a27fd80661b4cb41fddf81cbe9e3fa5
Shrejan Kotyan <shrejankotyan@gmail.com>
2026-03-19 09:41:02 +0530

    dummy

--- res.py
+++ res.py
@@ -3,6 +3,6 @@ import requests
 url = "http://localhost:8000/ocr"
 
 
-with open("test1.jpg", "rb") as f:
-    files = {"file": ("test1.jpg", f, "image/jpeg")}
+with open("imgs/test1.jpg", "rb") as f:
+    files = {"file": ("imgs/test1.jpg", f, "image/jpeg")}
     resp = requests.put(url, files=files)
