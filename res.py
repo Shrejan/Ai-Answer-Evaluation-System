96ce4f481da2587f9142eb8c09ff7abcece1453f
Shrejan Kotyan <shrejankotyan@gmail.com>
2026-04-02 15:39:17 +0530

    pulling

--- res.py
+++ res.py
@@ -2,8 +2,8 @@ import requests
 
 url = "http://localhost:8000/ocr"
 
-with open("imgs/test8.jpg", "rb") as f:
-    files = {"file": ("test8.jpg", f, "image/jpeg")}
+with open("imgs/test1.jpg", "rb") as f:
+    files = {"file": ("test1.jpg", f, "image/jpeg")}
     resp = requests.post(url, files=files)
 
 print(resp.status_code)
